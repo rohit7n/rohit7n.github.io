1b325c0eaa165da203a35c10d25a9c3920ffee25
Rohit <260186281+rohit7n@users.noreply.github.com>
2026-06-09 13:37:15 +0530

    Added Next version of minimalistic portfolio
    Clean , Simple but impactful

---
+++ script.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+document.querySelectorAll(".fade-in")
+.forEach(section => {
+    observer.observe(section);
+});
